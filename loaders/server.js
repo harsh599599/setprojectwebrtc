c44fbcec0c8a191ce86f2166c302163b70261f10
harsh599599 <harshthakkar599@gmail.com>
2020-12-29 18:25:02 +0530

    updatedsocketversion

--- loaders/server.js
+++ loaders/server.js
@@ -30,6 +30,7 @@ app
     .use(bodyParser.urlencoded({ extended: false }))
     .use(bodyParser.json())
     .use(function(req, res, next) {
+        console.log("ALLOW");
         res.setHeader("Access-Control-Allow-Origin", "*"); //access to which api
         res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept,Authorization");
         next();
